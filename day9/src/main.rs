0b6bd819dff95f98837964ff19f21cb5abc3d0c8
djavorszky <daniel.javorszky@gmail.com>
2020-12-09 21:25:29 +0100

    Day 9 - Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,11 +7,21 @@ fn main() -> Result<(), io::Error> {
 
     println!("v1: {}", result_v1);
 
+    let result_v2 = task_v2(&input).unwrap();
+
+    println!("v2: {}", result_v2);
+
     Ok(())
 }
 
 fn task_v1(input: &str) -> Option<u64> {
     find_first_wrong_number(25, &read_to_vec(input))
+}
+
+fn task_v2(input: &str) -> Option<u64> {
+    let numbers = read_to_vec(input);
+
+    find_weakness(&numbers, find_first_wrong_number(25, &numbers).unwrap())
 }
 
 fn read_to_vec(input: &str) -> Vec<u64> {
@@ -46,6 +56,25 @@ fn find_first_wrong_number(preamble_size: usize, numbers: &Vec<u64>) -> Option<u
     None
 }
 
+fn find_weakness(input: &Vec<u64>, target_number: u64) -> Option<u64> {
+    for (idx, _) in input.iter().enumerate() {
+        let mut numbers = Vec::new();
+        for num in &input[idx..] {
+            numbers.push(num);
+
+            let total: u64 = numbers.iter().map(|i| *i).sum::<u64>();
+            if total == target_number {
+                return Some(*numbers.iter().min().unwrap() + *numbers.iter().max().unwrap());
+            }
+
+            if total > target_number {
+                break;
+            }
+        }
+    }
+    None
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -75,4 +104,13 @@ mod tests {
         assert_eq!(find_first_wrong_number(2, &test_input).unwrap(), 9);
     }
 
+    #[test]
+    fn find_weakness_works() {
+        let input = vec![
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ];
+
+        assert_eq!(find_weakness(&input, 127).unwrap(), 62);
+    }
 }
