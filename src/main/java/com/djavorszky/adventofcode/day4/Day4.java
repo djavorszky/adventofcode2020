bf54ea0d36607073ea445d7d9fb76f345c27ab1d
djavorszky <daniel.javorszky@gmail.com>
2020-12-18 13:47:09 +0100

    Java - Day 4

--- src/main/java/com/djavorszky/adventofcode/day4/Day4.java
+++ src/main/java/com/djavorszky/adventofcode/day4/Day4.java
@@ -3,14 +3,26 @@ package com.djavorszky.adventofcode.day4;
 import com.djavorszky.adventofcode.Day;
 import com.djavorszky.adventofcode.helper.InputSource;
 
+import java.util.List;
+
 public class Day4 implements Day {
   @Override
   public Long task1(InputSource inputSource) {
-    return null;
+    List<String> condensedInput = MultiLineCondenser.condense(inputSource.getInput());
+
+    return condensedInput.parallelStream()
+        .map(Passport::valueOf)
+        .filter(Passport::isValidV1)
+        .count();
   }
 
   @Override
   public Long task2(InputSource inputSource) {
-    return null;
+    List<String> condensedInput = MultiLineCondenser.condense(inputSource.getInput());
+
+    return condensedInput.parallelStream()
+        .map(Passport::valueOf)
+        .filter(Passport::isValidV2)
+        .count();
   }
 }
