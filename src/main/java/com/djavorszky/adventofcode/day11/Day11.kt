1e90cd39f3ae1cb4c73385fdda4a7cb6a4a8ddc5
djavorszky <daniel.javorszky@gmail.com>
2020-12-23 12:46:59 +0100

    Kotilin - Day 11 Stub

---
+++ src/main/java/com/djavorszky/adventofcode/day11/Day11.kt
@@ -0,0 +1,14 @@
+package com.djavorszky.adventofcode.day11
+
+import com.djavorszky.adventofcode.Day
+import com.djavorszky.adventofcode.helper.InputSource
+
+class Day11 : Day {
+    override fun task1(inputSource: InputSource): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun task2(inputSource: InputSource): Long {
+        TODO("Not yet implemented")
+    }
+}
